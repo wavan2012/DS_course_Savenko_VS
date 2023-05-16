ed7d3e7708df0ae12699074ee4af61f704140bec
wavan2012 <wavan2012@gmail.com>
2023-05-16 18:32:41 +0300

    lesson_4 homework with init files

--- Homework/Lesson_4_Project/__init__.py
+++ Homework/Lesson_4_Project/__init__.py
@@ -1,3 +1,4 @@
 from Functions.Math_functions.function_2 import fun2
 from Functions.Math_functions.function_3 import fun3
 from Functions.list_in_string import lst_in_str
+from Data.lists_of_numbers import list_of_numbers2, list_of_numbers3
